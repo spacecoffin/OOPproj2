eb4c02bd4b42fdb961d2c83c135c033ed40904f2
spacecoffin <reed.rosenberg@gmail.com>
2014-03-26 14:56:07 -0400

    Added word validity check
    by way of a for loop that adds combinations of 2 or more consecutive
    alphabetic characters that have not already been added to word_list,
    the contents of which will later be written to disk as the dictionary.

--- buildDict.py
+++ buildDict.py
@@ -7,6 +7,11 @@
 #       "Dictionary" class in the "spellCheck" program.
 
 import re
+# import io
+# import string
+
+def reader():
+    pass
 
 def main():
         # The program buildDict should begin by asking the user for a
@@ -17,16 +22,31 @@ def main():
         "Please enter the name[s] of the file[s] (including the file-type \
         extension) from which to create the dictionary. \n\n If entering more \
         than one name, please separate each name by a space.\n\n"
-        ).split()
+        ).split() # formatting prints fucked up
     print(inputList)
+    # wordregex = re.compile('[a-z]{2,}?', flags=re.IGNORECASE)
+    word_list = []
     for f in inputList:
         try:
             file = open(f, 'r')
-            print(file.readline())
+            line = file.readline()
+            # print(line)            
+            # line_stream = io.StringIO(line)
+            # for c in line_stream:
+            split_line = re.split(r'[^a-z]+', line, flags=re.IGNORECASE)
+            for x in split_line:
+                if len(x) >= 2:
+                    if x not in word_list:
+                        word_list.append(x.lower())
+                
+            
+            
+            # print(linewords)
         except IOError:
             # FileNotFoundError IS NEW FOR 3.3! 3.2 uses IOError!
             # Old line: (FileNotFoundError, IOError, IsADirectoryError):
             print("***Unable to read file \'{}\'!***\n".format(f))
+    print(word_list)
 
     
     
