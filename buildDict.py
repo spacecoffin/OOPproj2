7a83fefb0acde67f96e31ee12dd3d89184846c52
spacecoffin <reed.rosenberg@gmail.com>
2014-03-26 01:36:04 -0400

    Added user prompted file read and read loop
    Prompts user to enter names of files to read into dictionary.
    For loop reads each file though no further operations are performed at
    this time.

--- buildDict.py
+++ buildDict.py
@@ -3,12 +3,32 @@
 # 3) Insert into dictionary (file, not object)
 
 # Assignment does not specify that this program should use classes.
-# It seems like it is meant to be used in the __init__ method of the "Dictionary" class in the "spellCheck" program.
+# This program is meant to be used in the __init__ method of the
+#       "Dictionary" class in the "spellCheck" program.
+
+import re
 
 def main():
-        #The program buildDict should begin by asking the user for a list of text files to read from. The user should respond by typing the file names as [filename1] [white space] [filename2] [white space] [filename3]...
-    inputList = input("Please enter the name[s] of the file[s] (including the file-type extension) from which to create the dictionary. \n\n If entering more than one name, please separate each name by a space.\n\n").split()
+        # The program buildDict should begin by asking the user for a
+        # list of text files to read from. The user should respond by
+        # typing the file names as [filename1] [white space] [filename2]
+        # [white space] [filename3]...
+    inputList = input(
+        "Please enter the name[s] of the file[s] (including the file-type \
+        extension) from which to create the dictionary. \n\n If entering more \
+        than one name, please separate each name by a space.\n\n"
+        ).split()
     print(inputList)
+    for f in inputList:
+        try:
+            file = open(f, 'r')
+            print(file.readline())
+        except IOError:
+            # FileNotFoundError IS NEW FOR 3.3! 3.2 uses IOError!
+            # Old line: (FileNotFoundError, IOError, IsADirectoryError):
+            print("***Unable to read file \'{}\'!***\n".format(f))
+
+    
     
 
 main()
